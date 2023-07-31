d2a04dd1df1bb7de932b8183d3394f74e499d13b
Moises Pahati <moisespahati@gmail.com>
2023-07-31 16:11:13 +0800

    add reducer function, add useEffect hook to fetchdata

--- src/App.js
+++ src/App.js
@@ -1,7 +1,52 @@
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+
+  //loading, error, ready, active, finished
+  status: 'loading',
+
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived': 
+      return {
+        ...state,
+        questions: action.payload,
+        status: 'ready',
+      }
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error"
+      }
+    
+    default:
+      throw new Error("Action unknown");
+      
+  }
+};
+
 export default function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    async function getQuestion() {
+      try {
+        const res = await fetch('http://localhost:9000/questions');
+        const data = await res.json();
+        dispatch({ type: 'dataReceived', payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFailed" });
+        console.log('nasalo ng catch ung error');
+      }
+    }
+    getQuestion();
+  }, []);
+
   return (
     <div className="app">
       <Header />
