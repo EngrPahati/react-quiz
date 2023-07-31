f8f9c2302a3ec4e4c0f9c3c8422d77dc38c495c7
Moises Pahati <moisespahati@gmail.com>
2023-08-01 00:02:27 +0800

    add NextButton component

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextButton from "./NextButton";
 
 const initialState = {
   questions: [],
@@ -12,6 +13,8 @@ const initialState = {
   //loading, error, ready, active, finished
   status: 'loading',
   index: 0,
+  answer: null,
+  points: 0,
 
 };
 
@@ -23,13 +26,30 @@ function reducer(state, action) {
         questions: action.payload,
         status: 'ready',
       }
+    
     case "dataFailed":
       return {
         ...state,
         status: "error"
       }
+    
     case "start":
-      return {...state, status: "active"}
+      return { ...state, status: "active" }
+    
+    case "newAnswer": 
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      }
+    
+    case "nextQuestion":
+      return {...state, index: state.index + 1, answer: null}
     
     default:
       throw new Error("Action unknown");
@@ -38,7 +58,7 @@ function reducer(state, action) {
 };
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer }, dispatch] = useReducer(reducer, initialState);
 
   const numQuestions = questions.length;
 
@@ -66,8 +86,19 @@ export default function App() {
           numQuestions={numQuestions}
           dispatch={dispatch}
         />}
-        {status === 'active' && <Question
-          question={questions[index]} />}
+        {status === 'active' &&
+          <>
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+            />
+          </>
+        }
 
       </Main>
     </div>
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,14 +1,32 @@
-import React from 'react'
 
-export default function Options() {
+export default function Options({ question, dispatch, answer }) {
+  const hasAnswered = answer !== null;
+
   return (
     <div className="options">
-        {question.options.map(option => (
-            <button
-                className="btn btn-option"
-                key={option}>
-                {option}
-            </button>
+        {question.options.map((option, index) => (
+          <button
+            onClick={() =>
+              dispatch({
+                type: "newAnswer",
+                payload: index,
+              })}
+            className={`
+            btn btn-option 
+            ${index === answer ? 'answer' : ""}
+            ${hasAnswered
+              ? index === question.correctOption
+                ? "correct"
+                : "wrong"
+              : ""
+                }`}
+            key={option}
+            disabled={hasAnswered}
+          >
+            
+            
+            {option}
+          </button>
         ))}
     </div>
   )
