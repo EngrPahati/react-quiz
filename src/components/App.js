0ed78225a26a77689b3baf2ccce3ddf1396c2ca7
Moises Pahati <moisespahati@gmail.com>
2023-08-02 13:59:40 +0800

    add Footer and Timer component

--- src/components/App.js
+++ src/components/App.js
@@ -8,6 +8,8 @@ import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
 import FinishScreen from "./FinishScreen";
+import Timer from "./Timer";
+import Footer from "./Footer";
 
 const initialState = {
   questions: [],
@@ -113,17 +115,20 @@ export default function App() {
             maxPossiblePoints={maxPossiblePoints}
             answer={answer}
           />
-            <Question
+          <Question
               question={questions[index]}
               dispatch={dispatch}
               answer={answer}
-            />
+          />
+          <Footer>
+            <Timer />
             <NextButton
               dispatch={dispatch}
               answer={answer}
               index={index}
               numQuestions={numQuestions}
             />
+          </Footer>
           </>
         }
 
