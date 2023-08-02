8be5e2f39237b03161225316e218550edd4b72cc
Moises Pahati <moisespahati@gmail.com>
2023-08-02 16:16:08 +0800

    add Footer and Timer component

---
+++ src/components/Timer.js
@@ -0,0 +1,17 @@
+import { useEffect } from "react"
+
+export default function Timer({dispatch, secondsRemaining}) {
+
+    useEffect( function() {
+        const id = setInterval(() => {
+            dispatch({type: 'tick'})
+        }, 1000);
+
+        return () => clearInterval(id);
+
+    }, [dispatch])
+
+  return (
+      <div className="timer">{secondsRemaining}</div>
+  )
+}
